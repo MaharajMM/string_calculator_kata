817a5b392a8cfc3fcaeeb989d659930e33eb8d74
Maharaj M M Behera <101055445+MaharajMM@users.noreply.github.com>
2025-02-17 19:48:56 +0530

    Step 2: Support single number input

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,8 +2,16 @@ import 'package:test/test.dart';
 import '../lib/string_calculator.dart';
 
 void main() {
-  test('Empty string returns 0', () {
-    final calculator = StringCalculator();
-    expect(calculator.add(""), equals(0));
+  group('StringCalculator', () {
+    test('Empty string returns 0', () {
+      final calculator = StringCalculator();
+      expect(calculator.add(""), equals(0));
+    });
+
+    test('Single number returns the number itself', () {
+      final calculator = StringCalculator();
+      expect(calculator.add("1"), equals(1));
+      expect(calculator.add("42"), equals(42));
+    });
   });
 }
